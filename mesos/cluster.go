52ab3071177a4076ba6eae5f954536c1dc9f4521
Andy Grunwald <andygrunwald@gmail.com>
2016-07-12 14:35:05 +0200

    Added strings.TrimSuffix to Master URL call

--- mesos/cluster.go
+++ mesos/cluster.go
@@ -27,8 +27,13 @@ type Cluster struct {
 
 // NewCluster creates a new cluster with the provided list of masters
 func NewCluster(masters []string) *Cluster {
+	var cleanMasters []string
+	for _, master := range masters {
+		cleanMasters = append(cleanMasters, strings.TrimSuffix(master, "/"))
+	}
+
 	return &Cluster{
-		masters: masters,
+		masters: cleanMasters,
 		client: http.Client{
 			Timeout: time.Second * 30,
 		},
